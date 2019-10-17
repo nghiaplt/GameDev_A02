ae9a6854a3db9f0ebfe54776b24259e13c7eadbe
nghia_pham_lt <tnghia14111998@gmail.com>
2019-10-17 23:50:59 +0700

    FC

---
+++ Assets/Scripts/CameraMove.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraMove : MonoBehaviour
+{
+    public Transform _target;
+    public Vector2 offset = new Vector2(2f, 1f);
+
+    int prevPos;
+    bool targetGoLeft;
+    // Start is called before the first frame update
+    void Start()
+    {
+        prevPos = Mathf.RoundToInt(_target.position.x);
+        transform.position = new Vector3(_target.position.x + offset.x, _target.position.y + offset.y, transform.position.z);
+    }
+
+    private void LateUpdate()
+    {
+        int curPos = Mathf.RoundToInt(_target.position.x);
+        if (curPos > prevPos)
+        {
+            targetGoLeft = false;
+        }
+        if(curPos < prevPos)
+        {
+            targetGoLeft = true;
+        }
+        prevPos = Mathf.RoundToInt(_target.position.x);
+        Vector3 cameraTarget;
+        cameraTarget = new Vector3(targetGoLeft ? _target.position.x - offset.x : _target.position.x + offset.x, _target.position.y + offset.y, transform.position.z);
+        Vector3 currentPosition = Vector3.Lerp(transform.position, cameraTarget, 1.5f * Time.deltaTime);
+        transform.position = currentPosition;
+    }
+}
---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,88 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+    public float walkPower = 150f;
+    public float jumpPower = 3f;
+    public AnimationClip walk, jump;
+    public Animation Legs;
+    public Transform ground;
+    public bool isGrounded;
+
+    private float distToGround;
+    private Vector2 inputAxis;
+    private Rigidbody2D rig;
+    private float startScale;
+    private bool isJump;
+    private bool isWalk;
+    private int walkAxis;
+    private bool canJump;
+    private bool canWalk;
+    public int countJump = 0;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        rig = gameObject.GetComponent<Rigidbody2D>();
+        startScale = transform.localScale.x;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if(isGrounded)
+        {
+            canJump = true;
+            canWalk = true;
+        }
+
+        if (canJump && ((Input.GetButtonDown("Jump")) || Input.GetKeyDown(KeyCode.Space)))
+        {
+            countJump = countJump + 1;
+            isJump = true;
+        }
+        if(Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.A))
+        {
+            isWalk = true;
+            if (Input.GetKey(KeyCode.D)) walkAxis = 1;
+            else walkAxis = -1;
+        }
+    }
+
+    private void FixedUpdate()
+    {
+        if (isWalk)
+        {
+            if (walkAxis == 1)
+            {
+                transform.localScale = new Vector3(startScale, startScale, 1);
+            }
+            else
+            {
+                transform.localScale = new Vector3(-startScale, startScale, 1);
+            }
+            rig.velocity = new Vector2(walkAxis * walkPower * Time.deltaTime, rig.velocity.y);
+
+            if (canWalk)
+            {
+                Legs.clip = walk;
+                Legs.Play();
+            }
+            isWalk = false;
+        }
+        if (isJump)
+        {
+            rig.AddForce(Vector2.up * jumpPower, ForceMode2D.Impulse);
+            if(countJump == 2)
+            {
+                canJump = false;
+            }
+            Legs.clip = jump;
+            Legs.Play();
+            canWalk = false;
+            isJump = false;
+        }
+    }
+}
